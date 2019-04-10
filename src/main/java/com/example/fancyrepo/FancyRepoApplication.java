dd286b121cb931a0ffb06b87b031cf5cf8a3d529
quanticcc <quanticcc@gmail.com>
2019-04-10 13:53:06 +0300

    Initial commit

---
+++ src/main/java/com/example/fancyrepo/FancyRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fancyrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancyRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancyRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fancyrepo/FancyRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.fancyrepo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FancyRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
